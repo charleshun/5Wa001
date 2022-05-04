a7408f1b4a03a2885d6b2847e2420cd9a1bf5a35
RBS <ronai.balint@naih.hu>
2022-05-04 13:53:22 +0200

    Add project files.

---
+++ 5Wa001/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _5Wa001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
